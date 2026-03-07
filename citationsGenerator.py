e295b0d8eb22bf7d9012e8a6ea9694c029c8b0a5
abdoulayesow7x <psow.el45@gmail.com>
2026-03-07 22:54:10 +0100

    Initial commit: MotivZen v1.0

---
+++ citationsGenerator.py
@@ -0,0 +1,44 @@
+import requests
+import os
+from datetime import datetime
+
+url = "https://zenquotes.io/api/random"
+filePath = r'C:\Users\niizu\Documents\Dev\Python\LearnPy\Projects\citations'
+
+def get_quote():
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return f"{data[0]['q']} \n-{data[0]['a']}"
+    else:
+        print("Error: couldn't connect to API")
+        return None
+
+def download(content):
+    if not os.path.exists(filePath):
+        os.makedirs(filePath)
+    
+    time_str = datetime.now().strftime("%d-%m-%Y_%H%M%S")
+    fileName = f"citation_{time_str}.txt"
+    fullPath = os.path.join(filePath, fileName)
+    
+    with open(fullPath, 'w', encoding='utf-8') as file:
+        file.write(content)
+        print(f"File created\n")
+try:
+    while True:
+        print("--- Welcome to MotivZen 🛡️ ---\n")
+        option = input("Do you want a quote? (Y/N): ").upper()
+
+        if option == "Y":
+            quote = get_quote()
+            if quote:
+                print(f"\n{quote}\n")
+                download(quote)
+        elif option == "N" or option == "":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option, please type Y or N.")
+except KeyboardInterrupt:
+    print("\nGoodbye!")
